d7dae756918e5b0ae29520f743a145a9c14a9edf
zhanghui <mars.zhang@beyonds.com>
2021-03-05 18:05:38 +0800

    最小队列

---
+++ java/com/mars/leetCode/editor/cn/MinStack.java
@@ -0,0 +1,93 @@
+package com.mars.leetCode.editor.cn;
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+//
+// 
+// push(x) —— 将元素 x 推入栈中。 
+// pop() —— 删除栈顶的元素。 
+// top() —— 获取栈顶元素。 
+// getMin() —— 检索栈中的最小元素。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入：
+//["MinStack","push","push","push","getMin","pop","top","getMin"]
+//[[],[-2],[0],[-3],[],[],[],[]]
+//
+//输出：
+//[null,null,null,null,-3,null,0,-2]
+//
+//解释：
+//MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.getMin();   --> 返回 -2.
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// pop、top 和 getMin 操作总是在 非空栈 上调用。 
+// 
+// Related Topics 栈 设计
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack{
+    public static void main(String[] args){
+        Solution solution = new MinStack().new Solution();
+        solution.push(-3);
+        System.out.println(solution.top());
+        solution.pop();
+        System.out.println(solution.getMin());
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public Solution() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ java/com/mars/leetCode/editor/cn/MinStack2.java
@@ -0,0 +1,163 @@
+package com.mars.leetCode.editor.cn;
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+//
+// 
+// push(x) —— 将元素 x 推入栈中。 
+// pop() —— 删除栈顶的元素。 
+// top() —— 获取栈顶元素。 
+// getMin() —— 检索栈中的最小元素。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入：
+//["MinStack","push","push","push","getMin","pop","top","getMin"]
+//[[],[-2],[0],[-3],[],[],[],[]]
+//
+//输出：
+//[null,null,null,null,-3,null,0,-2]
+//
+//解释：
+//MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.getMin();   --> 返回 -2.
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// pop、top 和 getMin 操作总是在 非空栈 上调用。 
+// 
+// Related Topics 栈 设计
+
+public class MinStack2 {
+    public static void main(String[] args) {
+        Solution solution = new MinStack2().new Solution();
+        solution.push(395);
+        System.out.println(solution.getMin());
+        System.out.println(solution.top());
+        System.out.println(solution.getMin());
+        solution.push(276);
+        solution.push(29);
+        System.out.println(solution.getMin());
+        solution.push(-482);
+        System.out.println(solution.getMin());
+        solution.pop();
+        solution.push(-108);
+        solution.push(-251);
+        System.out.println(solution.getMin());
+        solution.push(-439);
+        System.out.println(solution.top());
+        solution.push(370);
+        solution.pop();
+        solution.pop();
+        System.out.println(solution.getMin());
+        solution.pop();
+        System.out.println(solution.getMin());
+        System.out.println(solution.getMin());
+        solution.pop();
+        System.out.println(solution.getMin());
+        solution.push(-158);
+        solution.push(82);
+        solution.pop();
+
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+    class Solution {
+        class Node<N> {
+
+            private Node<N> pre;
+            private Node<N> next;
+            N item;
+
+            public Node(Node<N> pre, Node<N> next, N item) {
+                this.pre = pre;
+                this.next = next;
+                this.item = item;
+            }
+        }
+
+        /**
+         * initialize your data structure here.
+         */
+        private Node<Integer> first;
+        private Node<Integer> last;
+        private Node<Integer> min;
+
+        public Solution() {
+
+        }
+
+
+        public void push(int x) {
+            if (first == null) {
+                first = new Node<>(null, null, x);
+                last = new Node<>(null, null, x);
+                min = new Node<>(null, null, x);
+            } else {
+                Node<Integer> node = new Node<>(null, first, x);
+                node.next = first;
+                first.pre = node;
+                first = node;
+
+                Node<Integer> min ;
+                if (first.item > x) {
+                    min = new Node<>(null, this.min, first.item);
+                    this.min.pre = min;
+                    this.min = min;
+                }else {
+                    min = new Node<>(null, this.min, this.min.item);
+                    this.min.pre = min;
+                    this.min = min;
+                }
+
+            }
+
+
+        }
+
+        public void pop() {
+            if (first == null) {
+                throw new IllegalArgumentException("the stack is empty");
+            }
+            first = first.next;
+            min = min.next;
+            if (first == null) {
+                this.last = null;
+            } else {
+                first.pre = null;
+            }
+        }
+
+        public int top() {
+            int node = first.item;
+            return node;
+        }
+
+        public int getMin() {
+            return min.item;
+        }
+    }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
